cdaa102e8a9deaad41f715b69d1f48a923dff70a
Igors78 <oleinikovsigors@gmail.com>
2021-11-04 20:40:09 +0200

    Add ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ioleinik <ioleinik@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 20:24:38 by ioleinik          #+#    #+#             */
+/*   Updated: 2021/11/04 20:32:13 by ioleinik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (1);
+	}
+	else
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
