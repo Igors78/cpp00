bccb420a6922ae64515065784eacfcaae633537d
Igors78 <oleinikovsigors@gmail.com>
2021-11-04 21:57:28 +0200

    Shorten to standard

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ioleinik <ioleinik@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 20:24:38 by ioleinik          #+#    #+#             */
-/*   Updated: 2021/11/04 21:48:47 by ioleinik         ###   ########.fr       */
+/*   Updated: 2021/11/04 21:56:34 by ioleinik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,14 @@
 
 int main(int argc, char **argv)
 {
-	int k;
-
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		for (int i = 1; i < argc; i++)
 		{
-			k = 0;
-			while (argv[i][k])
-			{
+			for (int k = 0; argv[i][k]; k++)
 				std::cout << (char)toupper(argv[i][k]);
-				k++;
-			}
 		}
 		std::cout << std::endl;
 	}
